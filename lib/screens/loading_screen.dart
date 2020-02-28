2f1c88a9fe2b12eae8a98d454cea932477a8fab2
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-02-28 19:51:07 +0300

    Added weather api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
+final apiKey = DotEnv().env['API_KEY'];
+
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Location location = Location();
+  Location location = Location();
+
+  Future<void> getLocation() async {
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+  }
+
+  void getData() async {
+    await getLocation();
+    try {
+      http.Response response = await http.get(
+          'http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
+      if (response.statusCode == 200) {
+        print(response.body);
+      } else {
+        print(response.statusCode);
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getData();
   }
 
   @override
@@ -26,7 +44,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            getLocation();
+            getData();
           },
           child: Text('Get Location'),
         ),
